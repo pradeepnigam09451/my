ec26a7fc77d987bf6ebf959a90642585158c1b05
pradeepnigam09451 <pradeepnigam9451@gmail.com>
2025-04-07 01:35:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// document.getElementById("contactForm").addEventListener("submit", function (e) {
+//          e.preventDefault();
+       
+//          const name = document.getElementById("name").value.trim();
+//          const email = document.getElementById("email").value.trim();
+//          const message = document.getElementById("message").value.trim();
+       
+//          if (name && email && message) {
+//            document.getElementById("formResponse").innerText = "Thanks for contacting me, " + name + "!";
+//            this.reset();
+//          } else {
+//            document.getElementById("formResponse").innerText = "Please fill out all fields.";
+//          }
+//        });
+       
